85865d5175e4f5572c3dc3f52f00f60e809503f0
helloitsjoe <jboyle617@gmail.com>
2022-08-22 23:21:43 -0400

    clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
 module.exports = {
   extends: 'helloitsjoe',
+  requireConfigFile: false,
 };
--- app.js
+++ app.js
@@ -1,41 +1,59 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
-const {
-  postHandler,
-  proxyHandler,
-  portMiddleware,
-  noop,
-  simpleCors,
-} = require('./middleware');
 
-const corsOptions = { origin: 'http://localhost:3000' };
+// Simplified version of what the 'cors' library is doing
+const simpleCors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('X-My-Personal-Header', 'You are great');
+  next();
+};
+
+// Handler for all POST requests
+const postHandler = (port) => (req, res) => {
+  const { body, headers: reqHeaders } = req;
+  const resHeaders = res.getHeaders();
+  res.json({ message: `Hello from ${port}!`, body, reqHeaders, resHeaders });
+};
 
 const makeApp = (port, serveFrontend) => {
   const app = express();
 
-  // Serve index.html/js
-  app.use(serveFrontend ? noop : express.static(__dirname));
+  if (serveFrontend) {
+    // Serve index.html/js
+    app.use(express.static(__dirname));
+  }
 
   // Parse body in POST requests
   app.use(express.json());
-
-  // Add port to request
-  app.use(portMiddleware(port));
 
   // No CORS protection on OPTIONS (pre-POST) requests just to
   // make the POST requests more obvious what's happening
   app.options('*', cors());
 
   // No CORS, this will only respond to requests from the same domain (port)
-  app.post('/api', postHandler);
+  app.post('/api', postHandler(port));
 
-  // This will respond to cross-origin requests that match corsOptions
-  app.post('/api_cors', cors(corsOptions), postHandler);
+  // This will respond to all cross-origin requests
+  app.post('/api_cors_true', cors({ origin: true }), postHandler(port));
+
+  // This will respond only to cross-origin requests that match localhost:3000
+  app.post(
+    '/api_cors_3000',
+    cors({ origin: 'http://localhost:3000' }),
+    postHandler(port)
+  );
 
   // Simple CORS without cors library (replace cors() with this to use it)
-  // app.post('/api_cors', simpleCors, postHandler);
+  app.post('/api_cors_simple', simpleCors, postHandler(port));
+
   // Proxy to get around CORS
-  app.post('/api_proxy', simpleCors(), proxyHandler);
+  app.post('/api_proxy', async (req, res) => {
+    const response = await axios.post('http://localhost:3001/api', req.body, {
+      headers: { ...req.headers, 'X-Proxied': 'Yup' },
+    });
+    res.json(response.data);
+  });
 
   app.listen(port, () => {
     console.log(
