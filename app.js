d65b925c4e07884f3cebf45fc18aca4d495de097
helloitsjoe <jboyle617@gmail.com>
2022-10-30 16:24:25 -0400

    remove api endpoint from ui server

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const makeApp = (port = 3000) => {
   app.use(express.static('static'));
   app.use(express.json());
 
-  app.post('/api', (req, res) => {
-    res
-      .status(200)
-      .json({ message: `${req.body.message} right back from the UI server!` });
-  });
-
   app.listen(port, () => {
     console.log('Serving UI on http://localhost:3000');
   });
