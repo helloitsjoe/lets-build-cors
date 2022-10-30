c2611d67adba922bd5d07ec38504a9aeb5f800a5
helloitsjoe <jboyle617@gmail.com>
2022-10-30 15:18:56 -0400

    tidy

--- api.js
+++ api.js
@@ -5,14 +5,7 @@ const makeApi = (port = 3001) => {
 
   app.use(express.json());
 
-  app.options('/api', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.send('OK');
-  });
-
   app.post('/api', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
     res
       .status(200)
       .json({ message: `${req.body.message} right back from the API!` });
